440a1bd8cebf0f5d825b28f751ffbb39b117cd99
Sarthak Sirohi <EmailSarthakSirohi@gmail.com>
2018-12-29 14:10:07 +0530

    Added Afternoon Greetings

---
+++ Hello Java/Hello Java/src/Greetings/GoodAfternoon.java
@@ -0,0 +1,11 @@
+package Greetings;
+
+public class GoodAfternoon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good Afternoon Shailendra");
+
+	}
+
+}
