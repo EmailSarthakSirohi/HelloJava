93a6852ee72bc27e9c245152a6013a85683a9ed2
Sarthak Sirohi <EmailSarthakSirohi@gmail.com>
2018-12-29 13:46:02 +0530

    Update Kaminey to Super Kaminey

--- Hello Java/Hello Java/src/Aggressions/Kaminey.java
+++ Hello Java/Hello Java/src/Aggressions/Kaminey.java
@@ -4,6 +4,6 @@ public class Kaminey {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Kaminey! Mein tera khoon pee jaunga");
+		System.out.println("Super Kaminey! Mein tera khoon pee jaunga");
 	}
 }
